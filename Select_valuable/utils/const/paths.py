cc406a4213b3068b1b6e888c83f38af16118b5b5
ChentaoWEI <465704379@qq.com>
2023-10-31 22:15:22 -0700

    update 3

--- Select_valuable/utils/const/paths.py
+++ Select_valuable/utils/const/paths.py
@@ -1,3 +1,3 @@
-labeled_data = '/Users/weichentao/Documents/USC/2023fall/540/project/select_valuable/first_labeled'
-saved_data_cleaned = '/Users/weichentao/Documents/USC/2023fall/540/project/select_valuable/cleaned'
-saved_models = '/Users/weichentao/Documents/USC/2023fall/540/project/select_valuable/models'
+labeled_data = '/Users/weichentao/Documents/USC/2023fall/540/project/select_valuable/first_labeled' # 里面放原始数据
+saved_data_cleaned = '/Users/weichentao/Documents/USC/2023fall/540/project/select_valuable/cleaned' # 里面放tfidf矩阵和incidence矩阵
+saved_models = '/Users/weichentao/Documents/USC/2023fall/540/project/select_valuable/models' # 里面放训练好的模型
