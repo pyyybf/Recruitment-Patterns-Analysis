12019f587f3bd7aeae159c3416defe121df2ee67
panyue <pyyybf@gmail.com>
2023-11-07 03:17:29 -0800

    clear dir data_sample and mkdir for every year

--- retrieve_y/test.py
+++ retrieve_y/test.py
@@ -8,9 +8,10 @@ from main import retrieve_y
 
 def get_samples(n_per_year):
     # 每年抽几个吧
+    fs.clear_dir(f"./data_sample")
     for year in [dir for dir in os.listdir("./data_txt") if not dir.startswith(".")]:
         # 删除现有的年份文件夹
-        fs.clear_dir(f"./data_sample/{year}")
+        os.mkdir(f"./data_sample/{year}")
 
         file_names = [file_name for file_name in os.listdir(f"./data_txt/{year}") if not file_name.startswith(".")]
         random.shuffle(file_names)
