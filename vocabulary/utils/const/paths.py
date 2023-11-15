5c3630cdbf39b5c5fc48926dbf1ee470021766a1
Alycia Qiu <129646186+alyciaqiu@users.noreply.github.com>
2023-11-15 00:04:34 -0800

    Create paths.py

---
+++ vocabulary/utils/const/paths.py
@@ -0,0 +1,2 @@
+cleaned_valuable = 'C:/Alycia/USC/2023Fall/ISE540/Project/tf-idf_vocabulary/test' # 里面放还未提取词汇的数据，注意是年份文件夹的父文件夹
+vocabulary = 'C:/Alycia/USC/2023Fall/ISE540/Project/tf-idf_vocabulary/test_vocabulary' # 里面放提取出的词汇表
