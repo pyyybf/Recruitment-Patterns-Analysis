79a61f64014a3f70307f88e832b35deb0ff12301
panyue <pyyybf@gmail.com>
2023-11-19 06:45:04 -0800

    add default sample dir

--- retrieve_y/test.py
+++ retrieve_y/test.py
@@ -7,9 +7,9 @@ from utils import fs
 from main import retrieve_recruit_info, match_recruit_info
 
 
-def get_samples(n_per_year):
+def get_samples(n_per_year, sample_dir="./data_sample"):
     # 每年抽几个吧
-    fs.clear_dir(f"./data_sample")
+    fs.clear_dir(sample_dir)
     for year in [dir for dir in os.listdir("./data_txt") if not dir.startswith(".")]:
         # 删除现有的年份文件夹
         os.mkdir(f"./data_sample/{year}")
@@ -17,11 +17,11 @@ def get_samples(n_per_year):
         file_names = [file_name for file_name in os.listdir(f"./data_txt/{year}") if not file_name.startswith(".")]
         random.shuffle(file_names)
         for file_name in file_names[:n_per_year]:
-            shutil.copy(f"./data_txt/{year}/{file_name}", f"./data_sample/{year}/{file_name}")
+            shutil.copy(f"./data_txt/{year}/{file_name}", f"{sample_dir}/{year}/{file_name}")
 
 
 if __name__ == "__main__":
-    # get_samples(10)
+    get_samples(10)
     # retrieve_recruit_info("./data_sample")
     match_recruit_info(base_dir="./unmatch_txt",
                        target_file="./test_number_match.csv",
