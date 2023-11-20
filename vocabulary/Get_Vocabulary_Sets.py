7b2238282f61920f088ba42f909e235195080c35
xizhu-lin <xizhulin@usc.edu>
2023-11-20 12:56:41 -0800

    Update Get_Vocabulary_Sets.py

--- vocabulary/Get_Vocabulary_Sets.py
+++ vocabulary/Get_Vocabulary_Sets.py
@@ -10,6 +10,7 @@ import json
 import ast
 import nltk
 from nltk.corpus import stopwords
+import shutil
 
 
 # ## Get and save vocabulary_sets
@@ -87,9 +88,25 @@ def get_vocabulary_sets(folder_path):
 # In[7]:
 
 
+def clear_dir(dir_path):
+    if os.path.exists(dir_path):
+        shutil.rmtree(dir_path)
+    os.makedirs(dir_path)
+
+
+# In[8]:
+
+
+clear_dir('vocabulary_sets')
+
+
+# In[9]:
+
+
 folder_path_list = ["data_split/train_data/2016", "data_split/train_data/2017", 
                     "data_split/train_data/2018", "data_split/train_data/2019", 
-                    "data_split/train_data/2020", "data_split/train_data/2021", "data_split/train_data/2022"]
+                    "data_split/train_data/2020", "data_split/train_data/2021", 
+                    "data_split/train_data/2022"]
 for folder_path in folder_path_list:
     get_vocabulary_sets(folder_path)
 
