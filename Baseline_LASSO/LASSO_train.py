506e9113c8f74f9207dc7234bdd6d952170a1f7e
xizhu-lin <xizhulin@usc.edu>
2023-11-23 00:24:11 -0800

    Update LASSO_train.py

--- Baseline_LASSO/LASSO_train.py
+++ Baseline_LASSO/LASSO_train.py
@@ -6,7 +6,6 @@
 
 from sklearn.linear_model import Lasso
 from sklearn.model_selection import GridSearchCV, train_test_split
-from sklearn.datasets import make_regression
 from sklearn.metrics import mean_squared_error
 import numpy as np
 import joblib
@@ -38,9 +37,9 @@ y_train = data[:, 0]
 
 
 lasso_model = Lasso()
-
+alpha = np.concatenate((np.linspace(0.0001, 0.001, 10), np.linspace(0.001, 0.01, 10)))
 # tune hyperparameters: alpha
-param_grid = {'alpha': [0.01, 0.1, 1, 10, 100, 1000]}
+param_grid = {'alpha': alpha}
 
 # GridSearch
 grid_search = GridSearchCV(estimator=lasso_model, param_grid=param_grid, 
@@ -57,6 +56,36 @@ print(f"Best Parameters: {best_params}")
 # In[6]:
 
 
+feature_coefficients = best_model.coef_
+selected_features = np.where(feature_coefficients != 0)[0]
+
+print("Features with Non-Zero Coefficients:")
+print(selected_features)
+
+
+# In[7]:
+
+
+y_train_pred = best_model.predict(X_train)
+print(y_train_pred)
+
+
+# In[8]:
+
+
+mse = mean_squared_error(y_train, y_train_pred)
+print(f"Mean Squared Error on Training Set: {mse}")
+
+
+# In[9]:
+
+
 # save model
 joblib.dump(best_model, save_model_path)
+
+
+# In[ ]:
+
+
+
 
