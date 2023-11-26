a4d405ab10cb0ce67e340bf2564f20c786dd6971
panyue <pyyybf@gmail.com>
2023-11-26 03:12:16 -0800

    reconstruct

--- vocabulary_tfidf/TFIDF.py
+++ vocabulary_tfidf/TFIDF.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import json
 from nltk.tokenize import word_tokenize
 from collections import Counter
@@ -14,14 +8,9 @@ import re
 import shutil
 from tqdm import tqdm
 
-data_base_dir = "./data_split/train_data"
+from utils import paths
+
 years = list(range(2016, 2022 + 1))
-whole_vocabulary_json_path = './word/Whole_Vocabulary.json'
-word_counts_total_json_path = './word_counts/word_counts_total.json'
-result_dir = './TFIDF/csv_file'
-
-
-# In[2]:
 
 
 def load_json_file(file_path):
@@ -33,27 +22,14 @@ def load_json_file(file_path):
     return data
 
 
-# In[3]:
-
-
 def save_json_file(file_path, data):
     with open(file_path, 'w') as file:
         json.dump(data, file)
 
 
-# In[4]:
-
-
-Vocabulary = load_json_file(whole_vocabulary_json_path)
+Vocabulary = load_json_file(paths.whole_vocabulary_json_path)
 result_csv_headers = ["FILE_NAME", "CIK", "YEAR"] + Vocabulary
-
-# In[5]:
-
-
-word_counts_total = load_json_file(word_counts_total_json_path)
-
-
-# In[6]:
+word_counts_total = load_json_file(paths.word_counts_total_json_path)
 
 
 def calculate_TF(file_path):
@@ -70,9 +46,6 @@ def calculate_TF(file_path):
     return TF_Dic
 
 
-# In[7]:
-
-
 def get_the_number_of_documents(folder_path):
     try:
         files = [file_name for file_name in os.listdir(folder_path) if file_name.endswith(".htm.txt")]
@@ -84,19 +57,13 @@ def get_the_number_of_documents(folder_path):
         return None
 
 
-# In[8]:
-
-
 number_of_documents_by_year = {}
 for year in range(2016, 2023):
     year = str(year)
-    number_of_documents = get_the_number_of_documents(f'{data_base_dir}/{year}')
+    number_of_documents = get_the_number_of_documents(f'{paths.data_base_dir}/{year}')
     number_of_documents_by_year[year] = number_of_documents
 
 number_of_documents_in_total = sum(number_of_documents_by_year.values())
-
-
-# In[9]:
 
 
 def calculate_IDF(word_counts_total, number_of_documents_in_total):
@@ -114,17 +81,10 @@ def calculate_IDF(word_counts_total, number_of_documents_in_total):
         return IDF_Dic
 
 
-# In[10]:
-
-
 IDF = calculate_IDF(word_counts_total, number_of_documents_in_total)
 
 
-# ## TFIDF Matrix
-
-# In[11]:
-
-
+# TFIDF Matrix
 def calculate_TFIDF(file_path, IDF):
     TF = calculate_TF(file_path)
 
@@ -133,24 +93,20 @@ def calculate_TFIDF(file_path, IDF):
     return TFIDF
 
 
-# In[12]:
-
-
 def get_cik(file_name):
     match = re.search(r'^(\d+)_', file_name)
     cik = int(match.group(1))
     return cik
 
 
-# In[13]:
-
 def save_TFIDF_to_csv(folder_path, IDF):
     year = folder_path[-4:]
     file_list = os.listdir(folder_path)
 
     text_files = [file for file in file_list if file.endswith(".txt")]
+    # text_files = sorted(text_files, key=lambda filename: int(filename.split("_")[0]))
 
-    with open(f"{result_dir}/{year}_TFIDF.csv", "w") as fp:
+    with open(f"{paths.result_dir}/{year}_TFIDF.csv", "w") as fp:
         writer = csv.DictWriter(fp, fieldnames=result_csv_headers)
         writer.writeheader()
 
@@ -199,30 +155,15 @@ def get_TFIDF(folder_path, IDF):
         save_json_file(TFIDF_path, TFIDF)
 
 
-# In[14]:
-
-
 def clear_dir(dir_path):
     if os.path.exists(dir_path):
         shutil.rmtree(dir_path)
     os.makedirs(dir_path)
 
 
-# In[15]:
+clear_dir(paths.result_dir)
 
-
-clear_dir(result_dir)
-
-# In[16]:
-
-
-folder_path_list = [f"{data_base_dir}/{year}" for year in years]
-# folder_path_list = ["data_split/train_data/2016", "data_split/train_data/2017",
-#                     "data_split/train_data/2018", "data_split/train_data/2019",
-#                     "data_split/train_data/2020", "data_split/train_data/2021",
-#                     "data_split/train_data/2022"]
+folder_path_list = [f"{paths.data_base_dir}/{year}" for year in years]
 for folder_path in folder_path_list:
     # get_TFIDF(folder_path, IDF)
     save_TFIDF_to_csv(folder_path, IDF)
-
-# In[ ]:
---
+++ vocabulary_tfidf/utils/paths.py
@@ -0,0 +1,4 @@
+data_base_dir = "./data_split/train_data"
+whole_vocabulary_json_path = "./word/Whole_Vocabulary.json"
+word_counts_total_json_path = "./word_counts/word_counts_total.json"
+result_dir = "./TFIDF"
