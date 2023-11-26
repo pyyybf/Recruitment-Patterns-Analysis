a956ed56e321d30d1aaaf422cfe5186a9090c6a1
panyue <pyyybf@gmail.com>
2023-11-26 03:00:02 -0800

    reconstruct

--- retrieve_y/retrieve_y.py
+++ retrieve_y/retrieve_y.py
@@ -2,23 +2,22 @@ import csv
 import os
 from tqdm import tqdm
 
-from utils.split_train_test import split_train_test, split_data_file
-from utils import fs
+from utils import fs, paths
 from utils.match_tool import match_employee_num
 from utils.retrieval_tool import Lines2Matrix, retrieve_top_n_idx, split_paragraph, merge_paragraph
-from utils.get_employee import get_employee, split_row_filled, split_row_unfilled
+from utils.get_employee import get_employee, get_change_rate
 
 
-def retrieve_recruit_info(base_dir="data_txt", top_n=5):
-    # 先读下数据文件
+def retrieve_recruit_info(base_dir=paths.data_txt_dir, top_n=5):
+    # Read labeled recruitment information sentences
     data_file_name = "recruit_number.csv"
     with open(f"./recruit_text/{data_file_name}", "r") as fp:
         lines = [row[0] for row in csv.reader(fp) if len(row) > 0 and len(row[0].strip()) > 0]
 
-    # 读取必须词 必须包含列表词之一否则0分
+    # Read required words, lines that do not contain any of the required words will be eliminated
     with open("./my_words/required_words.txt", "r") as fp:
         required_words = fp.read().strip().split()
-    # 读取关键词 包含就分很高
+    # Read keywords, lines that contain any of the keywords will be retrieved
     with open("./my_words/keywords.txt", "r") as fp:
         keywords = fp.read().strip().split()
 
@@ -29,21 +28,20 @@ def retrieve_recruit_info(base_dir="data_txt", top_n=5):
                                min_len=3)
     doc_inc_mat = transformer.fit_transform(lines)
 
-    # 开始遍历文件夹
     fs.clear_dir("./retrieve_results")
     for year in range(2016, 2023):
         os.mkdir(f"./retrieve_results/{year}")
 
-        file_names = fs.list_dir(f"./{base_dir}/{year}")
+        file_names = fs.list_dir(f"{base_dir}/{year}")
         with tqdm(total=len(file_names), unit="file",
                   desc=f"Retrieve Recruitment Information from Files of {year}") as pbar_retrieve:
             for file_name in file_names:
-                with open(f"./{base_dir}/{year}/{file_name}", "r") as fp:
+                with open(f"{base_dir}/{year}/{file_name}", "r") as fp:
                     lines = [line.strip() for line in fp.readlines() if len(line.strip()) > 0]
 
-                # 先把莫名分段拼回去
+                # Remove incorrect subparagraph
                 lines = merge_paragraph(lines)
-                # 再分个句 不行就去掉 泪目
+                # Split paragraph into sentences
                 lines = split_paragraph(lines)
 
                 cur_inc_mat = transformer.transform(lines)
@@ -55,27 +53,28 @@ def retrieve_recruit_info(base_dir="data_txt", top_n=5):
                 pbar_retrieve.update(1)
 
 
-def match_recruit_info(base_dir="data_txt", target_file="./number_match.csv", record_file="./employee_lines.txt"):
+def match_recruit_info(base_dir, target_file, record_file):
+    # Read ban words, lines that contain any ban word will be eliminated
     with open("./my_words/ban_words.txt", "r") as fp:
         ban_words = set([line.strip() for line in fp.readlines()])
 
-    # 清理下记录文件
+    # Clear the record files
     with open(record_file, "w") as fp:
         fp.write("")
     with open(target_file, "w") as fp:
         fp.write("year,file_name,employee_num\n")
 
     for year in range(2016, 2023):
-        file_names = fs.list_dir(f"./{base_dir}/{year}")
+        file_names = fs.list_dir(f"{base_dir}/{year}")
         with tqdm(total=len(file_names), unit="file",
                   desc=f"{year}") as pbar_retrieve:
             for file_name in file_names:
-                with open(f"./{base_dir}/{year}/{file_name}", "r") as fp:
+                with open(f"{base_dir}/{year}/{file_name}", "r") as fp:
                     lines = [line.strip() for line in fp.readlines() if len(line.strip()) > 0]
 
-                # 先把莫名分段拼回去
+                # Remove incorrect subparagraph
                 lines = merge_paragraph(lines)
-                # 再分个句 不行就去掉 泪目
+                # Split paragraph into sentences
                 lines = split_paragraph(lines)
 
                 employee_num = match_employee_num(lines, year, record_file, ban_words)
@@ -88,11 +87,17 @@ def match_recruit_info(base_dir="data_txt", target_file="./number_match.csv", re
 
 
 if __name__ == "__main__":
+    # Clean output files
+    fs.clear_dir(paths.output_dir)
+    # Retrieve recruitment info with tf-idf matrix, failed
     # retrieve_recruit_info(top_n=10)
-    match_recruit_info()
-    get_employee()
-    split_train_test()
-    split_data_file()
-    for prefix in ["train", "test"]:
-        split_row_filled(prefix=prefix)
-        split_row_unfilled(prefix=prefix)
+    # Match recruitment info with regular expression into number_match.csv, record matched lines in employee_lines.txt
+    match_recruit_info(base_dir=paths.data_txt_dir,
+                       target_file=f"{paths.output_dir}/number_match.csv",
+                       record_file=f"{paths.output_dir}/employee_lines.txt")
+    # Generate employee_num.json from number_match.csv to easily get number by cik and year
+    get_employee(csv_file=f"{paths.output_dir}/number_match.csv", output_file=f"{paths.output_dir}/employee_num.json")
+    # Calculate change rates and generate change_rate.csv
+    get_change_rate(source_file=f"{paths.output_dir}/employee_num.json",
+                    target_file=f"{paths.output_dir}/change_rate.csv",
+                    tmp_output_dir=paths.output_dir)
--- retrieve_y/utils/get_employee.py
+++ retrieve_y/utils/get_employee.py
@@ -1,10 +1,15 @@
 import csv
 import json
 import numpy as np
+import random
+import shutil
+import os
+
+from utils import fs
 
 
 # Create a dictionary to store the information
-def get_employee(csv_file="./number_match.csv", output_file="./employee_num.json"):
+def get_employee(csv_file, output_file):
     data = {}
 
     # Read the CSV file
@@ -38,23 +43,110 @@ def get_employee(csv_file="./number_match.csv", output_file="./employee_num.json
         json_file.write(json_data)
 
 
-# def get_change_rate(source_file="./employee_num.json", target_file="./change_rate.csv"):
-#     with open(source_file, "r") as file:
-#         employee_num_data = json.load(file)
-#
-#     with open(target_file, "w") as fp:
-#         fp.write("cik,from_year,from_num,to_year,to_num,change_rate,from_filename,to_filename\n")
-#         for cik, data in employee_num_data.items():
-#             years = sorted(data)
-#             for i in range(1, len(years)):
-#                 from_file = data[years[i - 1]]["file_name"]
-#                 to_file = data[years[i]]["file_name"]
-#
-#                 from_num = data[years[i - 1]]["employee_num"]
-#                 to_num = data[years[i]]["employee_num"]
-#                 change_rate = (to_num - from_num) / (from_num if from_num > 0 else 1)
-#
-#                 fp.write(f"{cik},{years[i - 1]},{from_num},{years[i]},{to_num},{change_rate},{from_file},{to_file}\n")
+def get_change_rate(source_file, target_file, tmp_output_dir):
+    with open(source_file, "r") as file:
+        employee_num_data = json.load(file)
+
+    time_num_headers = ["cik"]
+    time_num_headers += [str(year) for year in range(2016, 2023)]
+    time_num_headers += [f"{year}_file" for year in range(2016, 2023)]
+
+    with open(f"{tmp_output_dir}/time_num.csv", "w") as time_num_csv:
+        time_num_csv.write(f"{','.join(time_num_headers)}\n")
+        for cik, data in employee_num_data.items():
+            nums = []
+            file_names = []
+            pos_count = 0
+            for year in range(2016, 2023):
+                num = ""
+                file_name = ""
+                if str(year) in data:
+                    pos_count += 1
+                    num = str(data[str(year)]["employee_num"])
+                    file_name = data[str(year)]["file_name"]
+                nums.append(num)
+                file_names.append(file_name)
+            # Average annual employee number is larger 10, and employee number must be available for years 21 and 22
+            if sum([int(num or 0) for num in nums]) / pos_count > 10 and nums[-1] != "" and nums[-2] != "":
+                time_num_csv.write(f"{cik},{','.join(nums)},{','.join(file_names)}\n")
+
+    data = []
+    with open(f"{tmp_output_dir}/time_num.csv", "r") as fp:
+        rows = csv.DictReader(fp)
+        for row in rows:
+            row_data = {
+                "cik": row["cik"],
+                "2016_num": row["2016"],
+                "2016_file": row["2016_file"]
+            }
+            for year in range(2017, 2023):
+                row_data[f"{year}_num"] = row[str(year)]
+                row_data[f"{year}_file"] = row[f"{year}_file"]
+                pre_num = row[str(year - 1)]
+                cur_num = row[str(year)]
+                if pre_num == "" or cur_num == "":
+                    change_rate = ""
+                else:
+                    pre_num = int(pre_num)
+                    cur_num = int(cur_num)
+                    if cur_num == pre_num:
+                        change_rate = 0
+                    elif pre_num == 0:
+                        change_rate = 1
+                    else:
+                        change_rate = (cur_num - pre_num) / pre_num
+                row_data[str(year)] = change_rate
+            data.append(row_data)
+
+    fieldnames = ["cik"]
+    fieldnames += [str(year) for year in range(2017, 2023)]
+    fieldnames += [f"{year}_num" for year in range(2016, 2023)]
+    fieldnames += [f"{year}_file" for year in range(2016, 2023)]
+    with open(target_file, 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(data)
+
+
+def split_train_test(full_data_path, output_dir):
+    data = []
+    with open(full_data_path, newline="") as csvfile:
+        reader = csv.DictReader(csvfile)
+        fieldnames = reader.fieldnames
+        for row in reader:
+            data.append(row)
+
+    # Split training and test sets
+    train_cnt = int(len(data) * 0.8)
+    random.seed(123)
+    random.shuffle(data)
+    train_set = data[:train_cnt]
+    test_set = data[train_cnt:]
+
+    with open(f"{output_dir}/train_change_rate.csv", "w", newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(train_set)
+
+    with open(f"{output_dir}/test_change_rate.csv", "w", newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(test_set)
+
+
+def split_data_file(source_dir, target_dir, index_dir):
+    for prefix in ["train", "test"]:
+        fs.clear_dir(f"{target_dir}/{prefix}_data")
+        for year in range(2016, 2023):
+            os.makedirs(f"{target_dir}/{prefix}_data/{year}")
+        with open(f"{index_dir}/{prefix}_change_rate.csv", "r") as fp:
+            rows = csv.DictReader(fp)
+            for row in rows:
+                for year in range(2016, 2023):
+                    file_name = row[f"{year}_file"]
+                    if file_name != "":
+                        shutil.copy(f"{source_dir}/{year}/{file_name}",
+                                    f"{target_dir}/{prefix}_data/{year}/{file_name}")
 
 
 def split_row_filled(prefix="train"):
@@ -81,10 +173,10 @@ def split_row_filled(prefix="train"):
                     writer.writerow(row_data)
 
 
-def split_row_unfilled(prefix="train"):
-    with open(f"./{prefix}_change_rate.csv", "r") as source:
+def split_row_unfilled(output_dir, prefix="train"):
+    with open(f"{output_dir}/{prefix}_change_rate.csv", "r") as source:
         rows = csv.DictReader(source)
-        with open(f"./{prefix}_change_rate_unfilled.csv", "w") as target:
+        with open(f"{output_dir}/{prefix}_change_rate_unfilled.csv", "w") as target:
             writer = csv.DictWriter(target, fieldnames=["cik", "file_name", "year", "change_rate"])
             writer.writeheader()
 
@@ -98,10 +190,3 @@ def split_row_unfilled(prefix="train"):
                             "change_rate": row[str(year)],
                         }
                         writer.writerow(row_data)
-
-
-if __name__ == "__main__":
-    csv_file = "C:/Alycia/USC/2023Fall/ISE540/Project/retrieve_y_result/test_csv/result.csv"
-    output_file = "C:/Alycia/USC/2023Fall/ISE540/Project/retrieve_y_result/test_csv/results.json"
-    get_employee(csv_file, output_file)
-    print("All done!")
