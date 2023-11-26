28e1d23421382b33eb85df227a5f5cc235c68eab
Yunlong Li <tyrzaxl@gmail.com>
2023-11-26 15:49:57 -0800

    Cleaned

--- tf_idf_time_series/get_cik.py
+++ tf_idf_time_series/get_cik.py
@@ -24,42 +24,6 @@ def get_cik_from_csv(filename, column_name = 'CIK', chunk_size=1000):
 
     return int_set
 
-def read_file_to_set(file_path):
-    with open(file_path, 'r') as file:
-        return {int(line.strip()) for line in file}
-
-def process_and_save_csv(file_path, output_file_path, column_name="CIK", chunk_size=500):
-    chunk_iterator = pd.read_csv(file_path, chunksize=chunk_size)
-    first_chunk = True
-
-    for chunk in chunk_iterator:
-        chunk.dropna(how='all', inplace=True)
-        chunk[column_name] = chunk[column_name].astype(int)
-
-        if first_chunk:
-            chunk.to_csv(output_file_path, index=False)
-            first_chunk = False
-        else:
-            chunk.to_csv(output_file_path, mode='a', header=False, index=False)
-
-def match_intersect_cik(file_path, output_file_path, cik_set, chunk_size=500):
-    chunk_iterator = pd.read_csv(file_path, chunksize=chunk_size)
-    first_chunk = True
-
-    for chunk in chunk_iterator:
-        # Drop unwanted columns
-        chunk.drop(columns=['YEAR', 'FILE_NAME'], inplace=True, errors='ignore')
-
-        chunk = chunk[chunk['CIK'].isin(cik_set)]
-
-        # Write the processed chunk to the output file
-        if first_chunk:
-            chunk.to_csv(output_file_path, index=False)
-            first_chunk = False
-        else:
-            chunk.to_csv(output_file_path, mode='a', header=False, index=False)
-
-
 
 # Initialize the intersection set with the CIK values from the first CSV file
 print("Reading 2022 cik")
