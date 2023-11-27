66207710d1fde2270a7a4c2a48b32228f956ff47
panyue <pyyybf@gmail.com>
2023-11-26 16:04:50 -0800

    add comments

--- vocabulary_tfidf/utils/paths.py
+++ vocabulary_tfidf/utils/paths.py
@@ -1,12 +1,12 @@
-base_dir = "./data_split/train_data"
+base_dir = "./data_split/train_data"  # Directory for the base data (train/test)
 # base_dir = "./data_split/test_data"
 
-vocabulary_sets_dir = "./vocabulary_sets"
-vocabulary_word_dir = "./Vocabulary/word"
+vocabulary_sets_dir = "./vocabulary_sets"  # Directory for vocabulary
+vocabulary_word_dir = "./Vocabulary/word"  # Directory for vocabulary
 vocabulary_word_counts_dir = "./Vocabulary/word_counts"
 
-tfidf_dir = "./TFIDF"
-incmat_dir = "./Incidence_Matrix"
+tfidf_dir = "./TFIDF"  # Directory for the generated TFIDF matrices
+incmat_dir = "./Incidence_Matrix"  # Directory for the generated Incidence matrices
 
-change_rates_dir = "./change_rates"
-y_X_dir = "./y_X"
+change_rates_dir = "./change_rates"  # Directory for the train/test_change_rate_unfilled.csv
+y_X_dir = "./y_X"  # Directory for the csv file including both X and y (change rate or binary labels)
