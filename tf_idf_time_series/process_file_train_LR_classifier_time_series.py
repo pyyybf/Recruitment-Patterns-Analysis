d275d06a59994533a61bbdd837d831b42d622369
Yunlong Li <tyrzaxl@gmail.com>
2023-11-26 21:10:01 -0800

    fix

--- tf_idf_time_series/process_file_train_LR_classifier_time_series.py
+++ tf_idf_time_series/process_file_train_LR_classifier_time_series.py
@@ -2,18 +2,19 @@ import pandas as pd
 import numpy as np
 import os
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.model_selection import cross_val_score
 import joblib
 
 os.chdir("C:/Users/tyrza/OneDrive/Documents/USC/Fall2023/ISE540/Project/train_TFIDF/")
 
-y = pd.read_csv("train_label/label_2022.csv")['label_2022']
+y = pd.read_csv("train_TFIDF/train_label/label_2022.csv")['label_2022']
 y = y.to_list()
 
 def csv_to_numpy_without_column(csv_file_path, column_to_delete="CIK"):
     # Read the CSV file into a DataFrame
     df = pd.read_csv(csv_file_path)
-
+    df = df.sort_values(by='CIK')
     # Delete the specified column
     if column_to_delete in df.columns:
         df.drop(column_to_delete, axis=1, inplace=True)
@@ -27,29 +28,29 @@ def csv_to_numpy_without_column(csv_file_path, column_to_delete="CIK"):
 # Usage
 column_to_delete = 'CIK'
 print("Reading 2016")
-csv_to_numpy_without_column('filtered_matched_2016_TFIDF.csv')
+csv_to_numpy_without_column('matched_2016_TFIDF.csv')
 print("Reading 2017")
-csv_to_numpy_without_column('filtered_matched_2017_TFIDF.csv')
+csv_to_numpy_without_column('matched_2017_TFIDF.csv')
 print("Reading 2018")
-csv_to_numpy_without_column('filtered_matched_2018_TFIDF.csv')
+csv_to_numpy_without_column('matched_2018_TFIDF.csv')
 print("Reading 2019")
-csv_to_numpy_without_column('filtered_matched_2019_TFIDF.csv')
+csv_to_numpy_without_column('matched_2019_TFIDF.csv')
 print("Reading 2020")
-csv_to_numpy_without_column('filtered_matched_2020_TFIDF.csv')
+csv_to_numpy_without_column('matched_2020_TFIDF.csv')
 print("Reading 2021")
-csv_to_numpy_without_column('filtered_matched_2021_TFIDF.csv')
+csv_to_numpy_without_column('matched_2021_TFIDF.csv')
 print("Reading 2022")
-csv_to_numpy_without_column('filtered_matched_2022_TFIDF.csv')
+csv_to_numpy_without_column('matched_2022_TFIDF.csv')
 
-tfidf2016 = np.load('array_filtered_matched_2016_TFIDF.npy')
-tfidf2017 = np.load('array_filtered_matched_2017_TFIDF.npy')
-tfidf2018 = np.load('array_filtered_matched_2018_TFIDF.npy')
-tfidf2019 = np.load('array_filtered_matched_2019_TFIDF.npy')
-tfidf2020 = np.load('array_filtered_matched_2020_TFIDF.npy')
-tfidf2021 = np.load('array_filtered_matched_2021_TFIDF.npy')
-tfidf2022 = np.load('array_filtered_matched_2022_TFIDF.npy')
+tfidf2016 = np.load('array_matched_2016_TFIDF.npy')
+tfidf2017 = np.load('array_matched_2017_TFIDF.npy')
+tfidf2018 = np.load('array_matched_2018_TFIDF.npy')
+tfidf2019 = np.load('array_matched_2019_TFIDF.npy')
+tfidf2020 = np.load('array_matched_2020_TFIDF.npy')
+tfidf2021 = np.load('array_matched_2021_TFIDF.npy')
+tfidf2022 = np.load('array_matched_2022_TFIDF.npy')
 
-parameter_list = [0.5,0.6,0.7,0.8,0.9]
+parameter_set = [0.5,0.6,0.7,0.8,0.9]
 best_parameter = 0
 best_mean_cv_score = 0
 
@@ -68,11 +69,10 @@ def get_X(parameter):
                                                (parameter**1)+1)
     return X
 
-for parameter in parameter_list:
+for parameter in parameter_set:
     print("Running model with",parameter,"as parameter")
     X = get_X(parameter)
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    model = LogisticRegression(C=1,solver='sag',penalty='l2', max_iter=1000,random_state=123)
+    model = LogisticRegression(solver='sag',penalty='l2',max_iter=1000,random_state=123)
     cv_scores = cross_val_score(model, X, y, cv=5,scoring='accuracy')
     print(cv_scores)
     print("Mean CV score is",cv_scores.mean())
@@ -80,7 +80,8 @@ for parameter in parameter_list:
     if cv_scores.mean() > best_mean_cv_score:
         best_mean_cv_score = cv_scores.mean()
         best_parameter = parameter
-        joblib.dump(model, 'model_LR_time_series.joblib')
 
-print("Best CV accuracy is", best_mean_cv_score)
-print("Best hyper parameter is", best_parameter)
+print("Best parameter is", best_parameter)
+X = get_X(best_parameter)
+model.fit(X,y)
+joblib.dump(model, 'model_LR_time_series.joblib')
---
+++ tf_idf_time_series/test.py
@@ -0,0 +1,25 @@
+import joblib
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+import pandas as pd
+
+
+print('Reading y')
+y_test = pd.read_csv("C:/Users/tyrza/OneDrive/Documents/USC/Fall2023/ISE540/Project/test_TFIDF/label.csv")['label']
+y_test = y_test.to_list()
+print('Reading X')
+X_test= pd.read_csv('C:/Users/tyrza/OneDrive/Documents/USC/Fall2023/ISE540/Project/test_TFIDF/test_2022_TFIDF.csv', header=None)
+X_test = X_test.to_numpy()
+print('Loading model')
+model = joblib.load('C:/Users/tyrza/OneDrive/Documents/USC/Fall2023/ISE540/Project/train_TFIDF/model/model_LR_time_series.joblib')
+print('Predicting')
+y_pred = model.predict(X_test)
+print('Getting scores...')
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+print(y_pred)
+print("Accuracy:",accuracy)
+print("Precision:",precision)
+print("Recall",recall)
+print("F1-score:",f1)
