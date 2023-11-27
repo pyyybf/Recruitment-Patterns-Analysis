fcc687d5cd3e433e25c5262834ef06d005734cf9
panyue <pyyybf@gmail.com>
2023-11-26 16:21:58 -0800

    add comments

--- topics/utils/paths.py
+++ topics/utils/paths.py
@@ -1,8 +1,8 @@
-lda_model_path = "./lda/trained_lda_model"
-id2word_path = "./lda/trained_lda_model.id2word"
+lda_model_path = "./lda/trained_lda_model"  # Path of trained LDA model
+id2word_path = "./lda/trained_lda_model.id2word"  # Path of id2word of trained LDA model
 
-train_data_path = "./data/train_data/train_topics.csv"
-test_data_path = "./data/test_data/test_topics.csv"
+train_data_path = "./data/train_data/train_topics.csv"  # Save path of training topic scores
+test_data_path = "./data/test_data/test_topics.csv"  # Save path of test topic scores
 
-save_model_dir = "./checkpoints"
-output_path = "./output.txt"
+save_model_dir = "./checkpoints"  # Save directory for trained models
+output_path = "./output.txt"  # Output file
