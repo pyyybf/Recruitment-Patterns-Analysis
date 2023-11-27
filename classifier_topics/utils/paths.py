045b0f7eb0796845028ecba1d05d75c1f843eed9
panyue <pyyybf@gmail.com>
2023-11-26 17:22:45 -0800

    reconstruct

--- classifier_topics/utils/paths.py
+++ classifier_topics/utils/paths.py
@@ -1,5 +1,5 @@
-train_data_path = "./topic_data/train_topic/train_topics.csv"  # Training topic scores
-test_data_path = "./topic_data/test_topic/test_topics.csv"  # Test topic scores
+train_data_path = "./data/train_topics.csv"  # Training dataset
+test_data_path = "./data/test_topics.csv"  # Test dataset
 
 save_model_dir = "./checkpoints"  # Save directory for trained models
 output_path = "./output.txt"  # Output file
